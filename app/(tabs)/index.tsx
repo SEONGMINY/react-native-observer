ce14f75c556f6095c481d6bf79bb76e737602618
SEONGMINY <leesm2075@naver.com>
2025-04-25 20:34:35 +0900

    fix: index 1부터 보이게 수정

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,26 +1,26 @@
-import {SafeAreaView, StyleSheet, Text, View} from "react-native";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import ObservedFlatList from "@/components/ObserverFlatList";
 import useInView from "@/hooks/useInView";
-import {ThemedText} from "@/components/ThemedText";
+import { ThemedText } from "@/components/ThemedText";
 
 export default function HomeScreen() {
   const items = new Array(50).fill(null).map((_, index) => ({
-    id: index.toString(),
+    id: (index + 1).toString(),
     title: `Item ${index + 1}`,
   }));
 
   return (
     <SafeAreaView>
       <ObservedFlatList
-          data={items}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item }) => <Item id={item.id} title={item.title} />}
+        data={items}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <Item id={item.id} title={item.title} />}
       />
     </SafeAreaView>
   );
 }
 
-const Item = ({ id, title }: { id: string, title: string }) => {
+const Item = ({ id, title }: { id: string; title: string }) => {
   useInView(() => {
     console.log(`${id}가 화면에 나타났어요!`);
 
@@ -30,9 +30,9 @@ const Item = ({ id, title }: { id: string, title: string }) => {
   }, [id]);
 
   return (
-      <View style={styles.item}>
-        <Text style={styles.title}>{title}</Text>
-      </View>
+    <View style={styles.item}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
   );
 };
 
